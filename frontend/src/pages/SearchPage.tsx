d25dcf0882dd1b989ea2c19336047730bc9aee33
frostyOtter <binary.hamlet147@gmail.com>
2024-06-22 01:09:03 +0700

    chores: remove unused error handle

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -20,7 +20,7 @@ const SearchPage = () => {
   const { searchQuery } = location.state as { searchQuery: string };
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
@@ -39,8 +39,6 @@ const SearchPage = () => {
         const data = await response.json();
         setSearchResults(data);
       } catch (error) {
-        setError(error.message);
-      } finally {
         setLoading(false);
       }
     };
@@ -81,92 +79,3 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
-
-
-// import { useLocation } from "react-router-dom";
-// import { useState, useEffect } from "react";
-
-
-// type SearchResult = {
-//     _index: string;
-//     _id: string;
-//     _score: number;
-//     _ignored?: string[];
-//     _source: {
-//         title: string;
-//         ingredients: string;
-//         time: number;
-//         cook: string;
-//         images: string;
-//     };
-// };
-
-// const SearchPage = () => {
-//     const location = useLocation();
-//     const { searchQuery } = location.state as { searchQuery: string };
-//     const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
-//     const [loading, setLoading] = useState<boolean>(true);
-//     const [error, setError] = useState<string | null>(null);
-
-//     useEffect(() => {
-//         const fetchSearchResults = async () => {
-//             try {
-//                 const response = await fetch(`http://localhost:8800/search_multi_features?user_input=${encodeURIComponent(searchQuery)}`, {
-//                     method: "POST",
-//                     headers: {
-//                         "Content-Type": "application/json",
-//                     },
-//                 });
-
-//                 if (!response.ok) {
-//                     throw new Error(`HTTP error! Status: ${response.status}`);
-//                 }
-
-//                 const data = await response.json();
-                
-//                 setSearchResults(data);
-//             } catch (error) {
-//                 setError(error.message);
-//             } finally {
-//                 setLoading(false);
-//             }
-//         };
-
-//         if (searchQuery) {
-//             fetchSearchResults();
-//         }
-//     }, [searchQuery]);
-
-//     if (loading) {
-//         return <div>Loading...</div>;
-//     }
-
-//     if (error) {
-//         return <div>Error: {error}</div>;
-//     }
-
-//     return (
-//         <div className="flex flex-col gap-12">
-//             <h1 className="text-3xl font-bold text-center">Search Results</h1>
-//             {searchResults.length > 0 ? (
-//                 <ul>
-//                     {searchResults.map((result) => (
-//                         <li key={result._id} className="border p-4 rounded shadow mb-4">
-//                             <h2 className="text-xl font-bold">{result._source.title}</h2>
-//                             <picture > 
-//                                 <source srcSet={result._source.images}/>
-//                             </picture>
-//                             <p><strong>Ingredients:</strong> {result._source.ingredients}</p>
-//                             <p><strong>Time:</strong> {result._source.time} minutes</p>
-//                             <p>{result._source.cook}</p>
-//                         </li>
-//                     ))}
-//                 </ul>
-//             ) : (
-//                 <div>Sorry, not found</div>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default SearchPage;
